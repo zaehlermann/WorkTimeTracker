468d5d5c444f1a6dbefec729ed41a52c10d2ea62
Mike Wähnert <mike.waehnert@iqvia.com>
2025-08-21 01:09:55 +0200

    main menue changed; TimeJournalView added.

--- timetracker-ui/src/main/java/de/zaehlermann/timetracker/base/ui/view/MainLayout.java
+++ timetracker-ui/src/main/java/de/zaehlermann/timetracker/base/ui/view/MainLayout.java
@@ -1,12 +1,23 @@
 package de.zaehlermann.timetracker.base.ui.view;
 
-import de.zaehlermann.timetracker.security.AppUserInfo;
-import de.zaehlermann.timetracker.security.CurrentUser;
+import static com.vaadin.flow.theme.lumo.LumoUtility.AlignItems;
+import static com.vaadin.flow.theme.lumo.LumoUtility.Display;
+import static com.vaadin.flow.theme.lumo.LumoUtility.FontSize;
+import static com.vaadin.flow.theme.lumo.LumoUtility.FontWeight;
+import static com.vaadin.flow.theme.lumo.LumoUtility.Gap;
+import static com.vaadin.flow.theme.lumo.LumoUtility.IconSize;
+import static com.vaadin.flow.theme.lumo.LumoUtility.Margin;
+import static com.vaadin.flow.theme.lumo.LumoUtility.Padding;
+import static com.vaadin.flow.theme.lumo.LumoUtility.TextColor;
+
+import java.io.Serial;
+
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.avatar.Avatar;
 import com.vaadin.flow.component.avatar.AvatarVariant;
+import com.vaadin.flow.component.contextmenu.MenuItem;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.icon.Icon;
@@ -22,69 +33,71 @@ import com.vaadin.flow.server.menu.MenuConfiguration;
 import com.vaadin.flow.server.menu.MenuEntry;
 import com.vaadin.flow.spring.security.AuthenticationContext;
 
-import static com.vaadin.flow.theme.lumo.LumoUtility.*;
+import de.zaehlermann.timetracker.security.AppUserInfo;
+import de.zaehlermann.timetracker.security.CurrentUser;
 
 @Layout
 @AnonymousAllowed // Allow all users, including anonymous ones. If you want only authenticated users, change to @PermitAll.
 public final class MainLayout extends AppLayout {
 
-    private final AuthenticationContext authenticationContext;
+  @Serial
+  private static final long serialVersionUID = -1005348531124747334L;
+  private final AuthenticationContext authenticationContext;
 
-    MainLayout(CurrentUser currentUser, AuthenticationContext authenticationContext) {
-        this.authenticationContext = authenticationContext;
-        setPrimarySection(Section.DRAWER);
-        addToDrawer(createHeader(), new Scroller(createSideNav()));
-        currentUser.get().ifPresent(user -> addToDrawer(createUserMenu(user)));
+  MainLayout(final CurrentUser currentUser, final AuthenticationContext authenticationContext) {
+    this.authenticationContext = authenticationContext;
+    setPrimarySection(Section.DRAWER);
+    addToDrawer(createHeader(), new Scroller(createSideNav()));
+    currentUser.get().ifPresent(user -> addToDrawer(createUserMenu(user)));
+  }
+
+  private Div createHeader() {
+    final Icon appLogo = VaadinIcon.CUBES.create();
+    appLogo.addClassNames(TextColor.PRIMARY, IconSize.LARGE);
+
+    final Span appName = new Span("TimeTrackerManager");
+    appName.addClassNames(FontWeight.SEMIBOLD, FontSize.LARGE);
+
+    final Div header = new Div(appLogo, appName);
+    header.addClassNames(Display.FLEX, Padding.MEDIUM, Gap.MEDIUM, AlignItems.CENTER);
+    return header;
+  }
+
+  private SideNav createSideNav() {
+    final SideNav nav = new SideNav();
+    nav.addClassNames(Margin.Horizontal.MEDIUM);
+    MenuConfiguration.getMenuEntries().forEach(entry -> nav.addItem(createSideNavItem(entry)));
+    return nav;
+  }
+
+  private SideNavItem createSideNavItem(final MenuEntry menuEntry) {
+    if(menuEntry.icon() != null) {
+      return new SideNavItem(menuEntry.title(), menuEntry.path(), new Icon(menuEntry.icon()));
+    }
+    else {
+      return new SideNavItem(menuEntry.title(), menuEntry.path());
+    }
+  }
+
+  private Component createUserMenu(final AppUserInfo user) {
+    final Avatar avatar = new Avatar(user.getFullName(), user.getPictureUrl());
+    avatar.addThemeVariants(AvatarVariant.LUMO_XSMALL);
+    avatar.addClassNames(Margin.Right.SMALL);
+    avatar.setColorIndex(5);
+
+    final MenuBar userMenu = new MenuBar();
+    userMenu.addThemeVariants(MenuBarVariant.LUMO_TERTIARY_INLINE);
+    userMenu.addClassNames(Margin.MEDIUM);
+
+    final MenuItem userMenuItem = userMenu.addItem(avatar);
+    userMenuItem.add(user.getFullName());
+    if(user.getProfileUrl() != null) {
+      userMenuItem.getSubMenu().addItem("View Profile",
+                                        event -> UI.getCurrent().getPage().open(user.getProfileUrl()));
     }
 
-    private Div createHeader() {
-        // TODO Replace with real application logo and name
-        var appLogo = VaadinIcon.CUBES.create();
-        appLogo.addClassNames(TextColor.PRIMARY, IconSize.LARGE);
-
-        var appName = new Span("Timetracker Ui");
-        appName.addClassNames(FontWeight.SEMIBOLD, FontSize.LARGE);
-
-        var header = new Div(appLogo, appName);
-        header.addClassNames(Display.FLEX, Padding.MEDIUM, Gap.MEDIUM, AlignItems.CENTER);
-        return header;
-    }
-
-    private SideNav createSideNav() {
-        var nav = new SideNav();
-        nav.addClassNames(Margin.Horizontal.MEDIUM);
-        MenuConfiguration.getMenuEntries().forEach(entry -> nav.addItem(createSideNavItem(entry)));
-        return nav;
-    }
-
-    private SideNavItem createSideNavItem(MenuEntry menuEntry) {
-        if (menuEntry.icon() != null) {
-            return new SideNavItem(menuEntry.title(), menuEntry.path(), new Icon(menuEntry.icon()));
-        } else {
-            return new SideNavItem(menuEntry.title(), menuEntry.path());
-        }
-    }
-
-    private Component createUserMenu(AppUserInfo user) {
-        var avatar = new Avatar(user.getFullName(), user.getPictureUrl());
-        avatar.addThemeVariants(AvatarVariant.LUMO_XSMALL);
-        avatar.addClassNames(Margin.Right.SMALL);
-        avatar.setColorIndex(5);
-
-        var userMenu = new MenuBar();
-        userMenu.addThemeVariants(MenuBarVariant.LUMO_TERTIARY_INLINE);
-        userMenu.addClassNames(Margin.MEDIUM);
-
-        var userMenuItem = userMenu.addItem(avatar);
-        userMenuItem.add(user.getFullName());
-        if (user.getProfileUrl() != null) {
-            userMenuItem.getSubMenu().addItem("View Profile",
-                    event -> UI.getCurrent().getPage().open(user.getProfileUrl()));
-        }
-        // TODO Add additional items to the user menu if needed
-        userMenuItem.getSubMenu().addItem("Logout", event -> authenticationContext.logout());
-
-        return userMenu;
-    }
+    userMenuItem.getSubMenu().addItem("Logout", event -> authenticationContext.logout());
+    return userMenu;
+  }
 
 }
--- timetracker-ui/src/main/java/de/zaehlermann/timetracker/base/ui/view/MainView.java
+++ timetracker-ui/src/main/java/de/zaehlermann/timetracker/base/ui/view/MainView.java
@@ -1,12 +1,14 @@
 package de.zaehlermann.timetracker.base.ui.view;
 
-import de.zaehlermann.timetracker.base.ui.component.ViewToolbar;
+import java.io.Serial;
 
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.Main;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.theme.lumo.LumoUtility;
+
+import de.zaehlermann.timetracker.base.ui.component.ViewToolbar;
 import jakarta.annotation.security.PermitAll;
 
 /**
@@ -16,18 +18,23 @@ import jakarta.annotation.security.PermitAll;
 @PermitAll // When security is enabled, allow all authenticated users
 public final class MainView extends Main {
 
-    // TODO Replace with your own main view.
+  @Serial
+  private static final long serialVersionUID = -5009310100331747758L;
 
-    MainView() {
-        addClassName(LumoUtility.Padding.MEDIUM);
-        add(new ViewToolbar("Main"));
-        add(new Div("Please select a view from the menu on the left."));
-    }
+  MainView() {
+    addClassName(LumoUtility.Padding.MEDIUM);
+    add(new ViewToolbar("Main"));
+    add(new Div("Welcome to the Time Tracker application!"));
+    add(new Div("1. Setup  your employee configuration."));
+    add(new Div("2. Manage your employees absence times."));
+    add(new Div("3. Scan your rfid tag to login/logout."));
+    add(new Div("4. Create time journal for an employee."));
+  }
 
-    /**
-     * Navigates to the main view.
-     */
-    public static void showMainView() {
-        UI.getCurrent().navigate(MainView.class);
-    }
+  /**
+   * Navigates to the main view.
+   */
+  public static void showMainView() {
+    UI.getCurrent().navigate(MainView.class);
+  }
 }
