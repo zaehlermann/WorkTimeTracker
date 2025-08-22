a8c5d0194fa7aa008c648d2db819bb6735e1afc2
Mike Wähnert <mike.waehnert@iqvia.com>
2025-08-22 22:22:19 +0200

    absence times

--- timetracker-rfid/src/main/java/de/zaehlermann/timetracker/model/Absence.java
+++ timetracker-rfid/src/main/java/de/zaehlermann/timetracker/model/Absence.java
@@ -7,18 +7,46 @@ import java.time.LocalTime;
  * One closed period of absence.
  * a.k.a. Abwesenheit
  */
-public class Absence {
+public class Absence implements AbstractCsvEntity<Employee> {
+  public static final String HEADER_LINE = "RFID;TYPE;STARTDAY;ENDDAY;STARTTIME;ENDTIME";
 
-  private String employeeId;
-  private AbsenceType type = AbsenceType.SICKNESS;
-  private LocalDate startDay = LocalDate.now();
+  private final String rfid;
+  private final AbsenceType type;
+  private final LocalDate startDay;
 
-  // null means the full start day only
-  private LocalDate endDay;
+  /**
+   * null means the full start day only
+   */
+  private final LocalDate endDay;
 
-  // Start/End null means the full day;
-  private LocalTime start;
-  private LocalTime end;
+  /**
+   * Start/End null means the full day;
+   */
+  private final LocalTime startTime;
+  /**
+   * Start/End null means the full day;
+   */
+  private final LocalTime endTime;
 
+  public Absence(final String rfid, final AbsenceType type,
+                 final LocalDate startDay, final LocalDate endDay,
+                 final LocalTime startTime, final LocalTime endTime) {
+    this.rfid = rfid;
+    this.type = type;
+    this.startDay = startDay;
+    this.endDay = endDay;
+    this.startTime = startTime;
+    this.endTime = endTime;
+  }
 
+  @Override
+  public String toCsvLine() {
+    return rfid + ";" +
+           type.name() + ";" +
+           startDay.toString() + ";" +
+           (endDay != null ? endDay.toString() : "") + ";" +
+           (startTime != null ? startTime.toString() : "") + ";" +
+           (endTime != null ? endTime.toString() : "") +
+           System.lineSeparator();
+  }
 }
