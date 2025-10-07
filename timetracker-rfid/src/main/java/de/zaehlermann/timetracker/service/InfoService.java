21ca56fca8be6eb87a6d64c8882970542af40908
zaehlermann <firebase.waehnert@gmail.com>
2025-10-07 20:05:09 +0200

    show version number in main menu

---
+++ timetracker-rfid/src/main/java/de/zaehlermann/timetracker/service/InfoService.java
@@ -0,0 +1,33 @@
+package de.zaehlermann.timetracker.service;
+
+import static java.util.Objects.requireNonNull;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+import javax.annotation.Nonnull;
+
+public class InfoService {
+
+  private String appVersion;
+
+  @Nonnull
+  public String getAppVersion() {
+    if(appVersion == null) {
+      appVersion = readAppVersion();
+    }
+    return appVersion;
+  }
+
+  @Nonnull
+  private String readAppVersion() {
+    try(InputStream resourceAsStream = getClass().getResourceAsStream("version.txt")) {
+      requireNonNull(resourceAsStream, "version file not found");
+      return new String(resourceAsStream.readAllBytes(), StandardCharsets.UTF_8);
+    }
+    catch(final IOException e) {
+      throw new IllegalStateException("Exception during reading app version", e);
+    }
+  }
+}
