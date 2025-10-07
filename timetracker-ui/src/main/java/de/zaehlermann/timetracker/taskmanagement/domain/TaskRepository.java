91be1c10ce099baf1247c00ba92df5fa4fc3694c
zaehlermann <firebase.waehnert@gmail.com>
2025-10-07 20:05:03 +0200

    clean code

--- timetracker-ui/src/main/java/de/zaehlermann/timetracker/taskmanagement/domain/TaskRepository.java
+++ timetracker-ui/src/main/java/de/zaehlermann/timetracker/taskmanagement/domain/TaskRepository.java
@@ -1,11 +1,13 @@
 package de.zaehlermann.timetracker.taskmanagement.domain;
 
+import java.io.Serializable;
+
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface TaskRepository extends JpaRepository<Task, Long>, JpaSpecificationExecutor<Task> {
+public interface TaskRepository extends JpaRepository<Task, Long>, JpaSpecificationExecutor<Task>, Serializable {
 
     // If you don't need a total row count, Slice is better than Page as it only performs a select query.
     // Page performs both a select and a count query.
--- timetracker-ui/src/main/java/de/zaehlermann/timetracker/taskmanagement/service/TaskService.java
+++ timetracker-ui/src/main/java/de/zaehlermann/timetracker/taskmanagement/service/TaskService.java
@@ -1,45 +1,50 @@
 package de.zaehlermann.timetracker.taskmanagement.service;
 
-import de.zaehlermann.timetracker.taskmanagement.domain.Task;
-import de.zaehlermann.timetracker.taskmanagement.domain.TaskRepository;
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.Clock;
+import java.time.LocalDate;
+import java.util.List;
+
 import org.jspecify.annotations.Nullable;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.Clock;
-import java.time.LocalDate;
-import java.util.List;
+import de.zaehlermann.timetracker.taskmanagement.domain.Task;
+import de.zaehlermann.timetracker.taskmanagement.domain.TaskRepository;
 
 @Service
 @PreAuthorize("isAuthenticated()")
-public class TaskService {
+public class TaskService implements Serializable {
 
-    private final TaskRepository taskRepository;
+  @Serial
+  private static final long serialVersionUID = 2931377461536510568L;
+  private final TaskRepository taskRepository;
 
-    private final Clock clock;
+  private final Clock clock;
 
-    TaskService(TaskRepository taskRepository, Clock clock) {
-        this.taskRepository = taskRepository;
-        this.clock = clock;
+  TaskService(final TaskRepository taskRepository, final Clock clock) {
+    this.taskRepository = taskRepository;
+    this.clock = clock;
+  }
+
+  @Transactional
+  public void createTask(final String description, @Nullable final LocalDate dueDate) {
+    if("fail".equals(description)) {
+      throw new RuntimeException("This is for testing the error handler");
     }
+    final Task task = new Task();
+    task.setDescription(description);
+    task.setCreationDate(clock.instant());
+    task.setDueDate(dueDate);
+    taskRepository.saveAndFlush(task);
+  }
 
-    @Transactional
-    public void createTask(String description, @Nullable LocalDate dueDate) {
-        if ("fail".equals(description)) {
-            throw new RuntimeException("This is for testing the error handler");
-        }
-        var task = new Task();
-        task.setDescription(description);
-        task.setCreationDate(clock.instant());
-        task.setDueDate(dueDate);
-        taskRepository.saveAndFlush(task);
-    }
-
-    @Transactional(readOnly = true)
-    public List<Task> list(Pageable pageable) {
-        return taskRepository.findAllBy(pageable).toList();
-    }
+  @Transactional(readOnly = true)
+  public List<Task> list(final Pageable pageable) {
+    return taskRepository.findAllBy(pageable).toList();
+  }
 
 }
