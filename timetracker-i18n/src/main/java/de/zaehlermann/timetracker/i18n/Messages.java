c0e9a24f40a28a0005c0845b94c89045516aae36
Zaehlermann <firebase.waehnert@gmail.com>
2026-01-04 20:56:25 +0100

    i18n module added

---
+++ timetracker-i18n/src/main/java/de/zaehlermann/timetracker/i18n/Messages.java
@@ -0,0 +1,23 @@
+package de.zaehlermann.timetracker.i18n;
+
+import javax.annotation.Nonnull;
+import java.text.MessageFormat;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class Messages {
+
+  @Nonnull
+  public static String get(@Nonnull final String key, @Nonnull final Object... args) {
+    return MessageFormat.format(get(key), args);
+  }
+
+  @Nonnull
+  public static String get(@Nonnull final String key) {
+    final ResourceBundle messages = ResourceBundle.getBundle(
+        "de.zaehlermann.timetracker.i18n.messages",
+        Locale.GERMAN);
+
+    return messages.getString(key);
+  }
+}
