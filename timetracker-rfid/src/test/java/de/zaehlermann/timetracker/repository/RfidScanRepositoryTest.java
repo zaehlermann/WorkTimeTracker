75d55337cc9f8407e18ef3ed666f85a455f53c6c
Zaehlermann <firebase.waehnert@gmail.com>
2026-01-14 00:33:06 +0100

    pom.xml cleaned; test enabled

--- timetracker-rfid/src/test/java/de/zaehlermann/timetracker/repository/RfidScanRepositoryTest.java
+++ timetracker-rfid/src/test/java/de/zaehlermann/timetracker/repository/RfidScanRepositoryTest.java
@@ -1,28 +1,25 @@
 package de.zaehlermann.timetracker.repository;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
 
 import java.time.Instant;
 import java.time.ZoneId;
 
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class RfidScanRepositoryTest {
 
   @Test
-  @Disabled("Only for manual testing, as it writes files")
   void saveScan() {
 
     final MutableClock clock = new MutableClock(Instant.parse("2024-06-01T00:00:00Z"), ZoneId.systemDefault());
     final RfidScanRepository repo = new RfidScanRepository(clock);
 
-    for(int i = 0; i < 100; i++) {
+    for (int i = 0; i < 100; i++) {
       repo.saveScan("1234567890");
-      if(i % 2 == 0) {
+      if (i % 2 == 0) {
         clock.plusDays(1);
-      }
-      else {
+      } else {
         clock.plusHours(7);
       }
     }
